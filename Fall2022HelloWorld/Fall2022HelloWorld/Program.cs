5d30eaf13e0fbef1ffc508d1471eb4185f727d80
Ashley <ashley.anne.parker@hotmail.com>
2022-09-07 15:38:16 -0400

    commit message

--- Fall2022HelloWorld/Fall2022HelloWorld/Program.cs
+++ Fall2022HelloWorld/Fall2022HelloWorld/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// What is this stuff?
+// What is a 'console' application?
+// What's with the fancy coloring?
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// Variables and Objects
+// Variable Declaration
+// Variable declaration is stating that we have a variable and giving it an associated name
+// if you are jus doing variable declaration, you must state the variable type first
+//
+// variableType variabelName;
+string Instructor;
+
+// Variable Assignment
+// Variable assignement is giving a variable a value of the appropiate type for us to reference later
+
+// variableName = value
+Instructor = "Gavin";
+// Variable Reassignment
+Instructor = "Davis";
+
+// var/variableType variabelName = value;
+// All together now!
+var Age = 24;
+// this will print out to the console showing the age 24 
+Console.WriteLine(Age);
+
+//Operators
+//Performs some kind of basic operation on some values
+// ex. 5+ 13 -> 18
+var studentCount = 5 + 13;
+Console.WriteLine(studentCount);
+// Aritmetic
+// Addition: +
+// Subtraction: -
+// Multiplication: *
+// Division: /
+var averageClassSize = studentCount / 3; 
+// Exponentiation: ^
+// Modulo: %
+// -- Returns the remainder after dividing by a particular number
