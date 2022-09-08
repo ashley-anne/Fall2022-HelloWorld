9ec40dc0169ce3cc419ebc23d31a60377f2c2324
Ashley <ashley.anne.parker@hotmail.com>
2022-09-08 13:09:54 -0400

    booleans and operators

--- Fall2022HelloWorld/Fall2022HelloWorld/Program.cs
+++ Fall2022HelloWorld/Fall2022HelloWorld/Program.cs
@@ -37,7 +37,79 @@ Console.WriteLine(studentCount);
 // Subtraction: -
 // Multiplication: *
 // Division: /
-var averageClassSize = studentCount / 3; 
-// Exponentiation: ^
+var averageClassSize = studentCount / 3;
+// Exponentiation: Math.Pow()
 // Modulo: %
 // -- Returns the remainder after dividing by a particular number
+
+//Booleans
+//Boolean is a data type where the only values are 'true' vs 'false' - 'on' vs 'off' - 1 vs 0
+var isSunny = true;
+
+// Equality
+// Checks to see if the value of instructor is the same as the value 'Davis'
+var isDavis = Instructor == "Davis"; // Davis == Davis -> true
+
+
+//Comparison
+// Checks to see if a value is greater than, less than or GE or LE
+// G: - >
+// L: - <
+//GE: >=
+// LE: <=
+var isGreaterThan = 25 >= 25; //checking is iterator greater than OR is iterator equal to?
+// Boolean Operators
+//We have some specific operators defined for actions between two boolean values
+
+//OR operator ||// The OR operator returns true i the left is true OR the right is true
+// true || false -> true
+// false || true -> true
+// true || true -> true
+// false || false -> false
+var willGoOutside = isSunny || isDavis; // true || true -> true
+// OR statements will run both parts of the code to return true
+
+
+
+// AND operator &&
+// The AND operator returns true if the left AND the right values are true
+// true && false -> true
+// false && true -> true
+// true && true -> true
+// false && false -> false
+var isRainy = true;
+var isCold = true;
+var willNotDrive = isRainy && isCold; // true && true  -> 
+
+// NOT operator !
+//Returns the opposite of a given boolean value
+// !true -> false
+// !false -> true
+var willDrive = !willNotDrive;
+
+//Not Equal !=
+// Checks to see if things are not equal
+
+// Exclusive OR (XOR) operator ^
+// Only returns true when the left or right are true, false when both are true;
+// we only want one side of our equation to be true both statements being true will result in the statement being false
+// true ^ false -> true
+// false ^ true -> true
+// true ^ true -> false
+// false ^ false -> false
+
+//Increment
+
+//Parsing
+// Transforming a variable from another type (often a string) into another (often an integer)
+var parsedValue = int.Parse("15"); // integer 15
+Console.WriteLine(parsedValue + 5);
+
+
+var isRed = false;
+var isBlue = true;
+var theSkyIs = isBlue; 
+var isGreen = false; 
+
+
+isRed && isBlue = theSkyIs
